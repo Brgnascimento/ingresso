a86cb84c314d4baa073963024c6721eddb5b06b5
Brendo Nascimento <brendoribeiro8@gmail.com>
2025-01-08 21:38:14 -0300

    Projeto inicial

---
+++ js/app.js
@@ -0,0 +1,50 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value =='pista'){
+        comprarPista(qtd)} else {
+            if (tipo.value == 'inferior'){
+                comprarInferior(qtd)
+            } else {
+                comprarSuperior(qtd)
+            }
+        }
+  
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent)
+
+    if(qtd > qtdPista){
+        alert('Não há vagas disponíveis para Pista!')
+    } else {
+        qtdPista = qtdPista - qtd
+        document.getElementById('qtd-pista').textContent = qtdPista
+        alert('Compra realizada com sucesso!')
+    } 
+}
+
+function comprarInferior(qtd){
+    let qtdinferior = parseInt(document.getElementById('qtd-inferior').textContent)
+
+    if(qtd > qtdinferior){
+        alert('Não há vagas disponíveis para Cadeira inferior!')
+    } else {
+        qtdinferior = qtdinferior - qtd
+        document.getElementById('qtd-inferior').textContent = qtdinferior
+        alert('Compra realizada com sucesso!')
+    } 
+}
+
+function comprarSuperior(qtd){
+    let qtdsuperior = parseInt(document.getElementById('qtd-superior').textContent)
+
+    if(qtd > qtdsuperior){
+        alert('Não há vagas disponíveis para Cadeira superior!')
+    } else {
+        qtdsuperior = qtdsuperior - qtd
+        document.getElementById('qtd-superior').textContent = qtdsuperior
+        alert('Compra realizada com sucesso!')
+    } 
+}
